b5d39328d4aca98e3eeea0098719b9c01afa00df
Oscar Canales <hoscar161@gmail.com>
2022-05-14 12:13:12 -0600

    creacion de la base de la api junto a sus variables de entorno

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require("express")
+const cors = require("express")
+const app = express()
+
+
+app.use(cors())
+
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log(`tu app esta lista por http://localhost:${port}`)
+})
